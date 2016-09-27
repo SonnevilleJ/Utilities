3fda10cc55e11894c3f73d0c7b39255cb719bbf4
John Sonneville <SonnevilleJ@gmail.com>
2016-09-26 23:23:33 -0500

    Testing mono CI build

---
+++ Sonneville.Utilities.Test/PlatformTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Sonneville.Utilities.Test
+{
+    [TestFixture]
+    public class PlatformTest
+    {
+        [Test]
+        public void FailIfOnMono()
+        {
+            if (Type.GetType("Mono.Runtime") != null)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
